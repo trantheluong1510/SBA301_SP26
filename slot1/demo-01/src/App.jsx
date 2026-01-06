ca930c55a67d7ff9a4b116d1d7a35ff133c27ac0
trantheluong1510 <de180077trantheluong@gmail.com>
2026-01-06 11:01:28 +0700

    add

---
+++ slot1/demo-01/src/App.jsx
@@ -0,0 +1,16 @@
+import Footer from './component/footer'
+import './App.css'
+import Header from './component/header';
+
+function App() {
+
+  return (
+    <div>
+      <Header></Header>
+      <h1>Hello</h1>
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App
